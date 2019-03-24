019f5c08b08df108e794a358c8889333d01b884b
Joe <assadammar@gmail.com>
2019-03-24 16:52:42 -0500

    typo fixed

--- peerdefine/peerDefine/settings.py
+++ peerdefine/peerDefine/settings.py
@@ -21,7 +21,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-with open(os.path.join(BASE_DIR, 'sensitiveData/secret_key.txt') as f):
+KEY_DIR = os.path.join(BASE_DIR, 'sensitiveData')
+KEYS_FILE = KEY_DIR + "/secret_key.txt"
+with open(KEYS_FILE) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
