e3223afab6f60e4eae1cf68b1005c16553478191
Joe <assadammar@gmail.com>
2019-03-24 19:53:02 -0500

    django-heroku package in requirements.txt

--- peerdefine/peerDefine/settings.py
+++ peerdefine/peerDefine/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku 
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -230,3 +232,6 @@ SECURE_SSL_REDIRECT = True
 X_FRAME_OPTIONS = 'DENY'
 
 MICROSOFT_KEY = os.getenv('Microsoft_Key')
+
+django_heroku.settings(locals())
+
