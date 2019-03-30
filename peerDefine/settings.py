329fcc6c1e25383250da1c3a6b41d23eb83e00da
Joe <assadammar@gmail.com>
2019-03-30 13:17:20 -0500

    debug = true

--- peerDefine/settings.py
+++ peerDefine/settings.py
@@ -29,7 +29,7 @@ KEYS_FILE = KEY_DIR + "/secret_key.txt"
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0', 'peerdefined.herokuapp.com', 'www.peerdefined.herokuapp.com']
 
