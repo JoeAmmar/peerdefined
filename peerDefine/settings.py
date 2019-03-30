526030fd7f14317db36247f7f7c3d60c817bec8d
Joe <assadammar@gmail.com>
2019-03-30 15:18:56 -0500

    changed database postgres

--- peerDefine/settings.py
+++ peerDefine/settings.py
@@ -127,14 +127,20 @@ WSGI_APPLICATION = 'peerDefine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'peerDefine',
+        'USER': os.getenv('dbUser'),
+        'PASSWORD': os.getenv('dbPas'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
